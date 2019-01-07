495743bc99316556cd3f581e019e2b44743c1730
Ivan Sharavuev <shpiwan@gmail.com>
2019-01-07 21:59:03 +0300

    Module create device. You can see it in /proc/devices.

--- buffer_driver.c
+++ buffer_driver.c
@@ -3,21 +3,30 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-static struct file_operations buffer_driver_fops;
-
 static const int MAJOR = 44;
 static const char DEVICE_NAME[] = "buffer-device";
 
-static int CONTAINING_VALUE_LEN = 1024;
-static char containing_value[CONTAINING_VALUE_LEN] = {""};
+#define CONTAINING_VALUE_LEN 1024
+static char containing_value[CONTAINING_VALUE_LEN];
 static ssize_t containing_value_len = 0;
+
+ssize_t buffer_driver_read(struct file *f, char __user *buf, size_t sz, loff_t *off);
+
+ssize_t buffer_driver_write(struct file *f, const char __user *buf, size_t sz, loff_t *off);
+
+static struct file_operations buffer_driver_fops =
+    {
+        .owner = THIS_MODULE,
+        .read = buffer_driver_read,
+        .write = buffer_driver_write,
+};
 
 static int __init buffer_init(void)
 {
     if (register_chrdev(MAJOR, DEVICE_NAME,
                         &buffer_driver_fops))
     {
-        printk(KERN_ERR, "buffer_driver: unable to get major %\d",MAJOR);
+        printk("buffer_driver: unable to get major\n");
         return -EIO;
     }
     return 0;
@@ -38,13 +47,6 @@ ssize_t buffer_driver_write(struct file *f, const char __user *buf, size_t sz, l
 {
     return 0;
 }
-
-buffer_driver_fops =
-    {
-        .owner = THIS_MODULE,
-        .read = buffer_driver_read,
-        .write = buffer_driver_write,
-};
 
 module_init(buffer_init);
 module_exit(buffer_exit);
