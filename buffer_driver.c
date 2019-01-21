22aef24a5596b737a4387db0a3e2cba7307f9e96
Ivan Sharavuev <shpiwan@gmail.com>
2019-01-21 22:49:36 +0300

    Create device in /dev

--- buffer_driver.c
+++ buffer_driver.c
@@ -1,11 +1,17 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/device.h>
 #include <linux/uaccess.h>
 #include <linux/module.h>
 
 static const int MAJOR = 44;
+static const int MINIR = 1;
 static const char DEVICE_NAME[] = "buffer-device";
+static const char DEVICE_CLASS[] = "buffer-device-class";
+
+static struct cdev hcdev;
+static struct class *devclass;
 
 #define CONTAINING_VALUE_LEN 1024
 #define CONTAINING_VALUE_USED_LEN ((CONTAINING_VALUE_LEN - 1)) // the last byte will be always '\0'
@@ -30,17 +36,22 @@ static int __init buffer_init(void)
         printk("buffer_driver: unable to get major\n");
         return -EIO;
     }
+    dev_t dev = MKDEV(MAJOR, MINIR);
+    devclass = class_create(THIS_MODULE, DEVICE_CLASS);
+    device_create(devclass, NULL, dev, NULL, "%s", DEVICE_NAME);
     return 0;
 }
 
 static void __exit buffer_exit(void)
 {
+    class_destroy(devclass);
     unregister_chrdev(MAJOR, DEVICE_NAME);
     return;
 }
 
 ssize_t buffer_driver_read(struct file *f, char __user *buf, size_t sz, loff_t *off)
 {
+    printk("[%s] Try to read from the device \n", DEVICE_NAME);
     int read_bytes = (CONTAINING_VALUE_USED_LEN < sz) ? CONTAINING_VALUE_USED_LEN : sz;
 
     return copy_to_user(buf, containing_value, read_bytes);
@@ -48,6 +59,7 @@ ssize_t buffer_driver_read(struct file *f, char __user *buf, size_t sz, loff_t *
 
 ssize_t buffer_driver_write(struct file *f, const char __user *buf, size_t sz, loff_t *off)
 {
+    printk("[%s] Try to write \'%s\' to the device \n", DEVICE_NAME, buf);
     int write_bytes = (CONTAINING_VALUE_USED_LEN < sz) ? CONTAINING_VALUE_USED_LEN : sz;
 
     return copy_from_user(containing_value, buf, write_bytes);
